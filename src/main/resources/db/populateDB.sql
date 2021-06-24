27469ce6381363697f096d7eff22d574f5059d85
Artyom Vasilyev <metagxd@gmail.com>
2021-06-24 13:58:36 +0300

    init & populate meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +10,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2020-01-30 10:00', 'Завтрак', 500),
+       (100000, '2020-01-30 13:15', 'Обед', 1000),
+       (100000, '2020-01-30 20:00', 'Ужин', 500),
+       (100001, '2020-01-31 10:00', 'Завтрак', 100),
+       (100001, '2020-01-31 13:00', 'Обед', 550),
+       (100001, '2020-01-31 20:00', 'Ужин', 500);
