b9719cdaddef594f5a55cd2197b768bccc2e4b8a
Artyom Vasilyev <metagxd@gmail.com>
2021-07-31 13:52:44 +0300

    tests refactor

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,15 +1,15 @@
 package ru.javawebinar.topjava.service.datajpa;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+import ru.javawebinar.topjava.service.MealService;
 
-import static ru.javawebinar.topjava.MealTestData.ADMIN_MEAL_ID;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.admin;
 
@@ -21,6 +21,7 @@ public class DataJpaMealServiceTest extends AbstractMealServiceTest {
     @Test
     public void getWithUser() {
         Meal mealWithUser = service.getWithUser(ADMIN_MEAL_ID, ADMIN_ID);
-        Assertions.assertThat(mealWithUser.getUser()).usingRecursiveComparison().ignoringFields("meals", "registered").isEqualTo(admin);
+        MATCHER.assertMatch(mealWithUser, adminMeal1);
+        UserTestData.MATCHER.assertMatch(mealWithUser.getUser(), admin);
     }
 }
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -1,11 +1,11 @@
 package ru.javawebinar.topjava.service.datajpa;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
@@ -17,6 +17,7 @@ import java.util.List;
 
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.admin;
 
 @ActiveProfiles(Profiles.DATAJPA)
 public class DataJpaUserServiceTest extends AbstractUserServiceTest {
@@ -31,7 +32,8 @@ public class DataJpaUserServiceTest extends AbstractUserServiceTest {
     public void getWithMeal() {
         User user = userService.getWithMeal(ADMIN_ID);
         List<Meal> adminMealList = Arrays.asList(adminMeal2, adminMeal1);
-        Assertions.assertThat(user.getMeals()).usingRecursiveComparison().ignoringFields("user").isEqualTo(adminMealList);
+        UserTestData.MATCHER.assertMatch(user, admin);
+        MATCHER.assertMatch(user.getMeals(), adminMealList);
     }
 
     @Test
